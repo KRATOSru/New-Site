54d92a0a70ccd35ed5eb046ea61c085858c2efbd
Владимир Картышов <KRATOS@beelinerouter.net>
2022-04-13 15:11:22 +0300

    app-bar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app> </v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" app temporary> </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
       <v-toolbar-title>Home page</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-app-bar-items class="hidden-sm-and-down">
+        <v-btn class="ma-1">Link one</v-btn>
+        <v-btn>Link one</v-btn>
+        <v-btn>Link one</v-btn>
+      </v-app-bar-items>
     </v-app-bar>
 
-    <v-main> </v-main>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
