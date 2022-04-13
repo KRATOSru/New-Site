04c7ddc10938645f3cff55c7bb20a3092a412a48
Владимир Картышов <KRATOS@beelinerouter.net>
2022-04-13 16:51:09 +0300

    navigation-drawer

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app temporary> </v-navigation-drawer>
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list>
+        <v-list-item-group>
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="'About me'"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
@@ -8,9 +21,26 @@
       <v-toolbar-title>Home page</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-app-bar-items class="hidden-sm-and-down">
-        <v-btn class="ma-1">Link one</v-btn>
-        <v-btn>Link one</v-btn>
-        <v-btn>Link one</v-btn>
+        <v-btn class="ml-1">
+          <v-icon left>mdi-account-school</v-icon>
+          Sertificates
+        </v-btn>
+        <v-btn class="ml-1">
+          <v-icon left>mdi-human-dolly</v-icon>
+          Works
+        </v-btn>
+        <v-btn class="ml-1">
+          <v-icon left>mdi-web</v-icon>
+          Vue
+        </v-btn>
+        <v-btn class="ml-1">
+          <v-icon left>mdi-apple</v-icon>
+          Apple
+        </v-btn>
+        <v-btn class="ml-1">
+          <v-icon left>mdi-account</v-icon>
+          About me
+        </v-btn>
       </v-app-bar-items>
     </v-app-bar>
 
