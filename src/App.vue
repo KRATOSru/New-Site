e7eb0bdda2c3f0e647175863c8025457763cfb0b
Владимир Картышов <KRATOS@beelinerouter.net>
2022-04-13 14:33:15 +0300

    wireframe

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
   <v-app>
-    <dir>jhhhj</dir>
-    <v-main>
-      <router-view />
-    </v-main>
+    <v-navigation-drawer v-model="drawer" app> </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Home page</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
+  data: () => ({drawer: null}),
 }
 </script>
