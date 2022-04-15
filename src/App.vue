6cfd263b111b1d2e7337cf018d8e3b3e086d3cce
Владимир Картышов <KRATOS@beelinerouter.net>
2022-04-15 05:16:10 +0300

    animation

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,19 @@
   <v-app>
     <v-navigation-drawer v-model="drawer" app temporary>
       <v-list>
-        <v-list-item-group
+       <v-list-item
           v-for="link in links"
           :key="link.title"
           :to="link.url"
         >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>{{ link.icon }}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-content>
-              <v-list-item-title v-text="link.title"></v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
+          <v-list-item-icon>
+            <v-icon>{{ link.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
@@ -57,7 +56,11 @@ export default {
     return {
       drawer: false,
       links: [
-        {title: 'Sertificates',icon: 'mdi-account-school',url: '/sertificates',},
+        {
+          title: 'Sertificates',
+          icon: 'mdi-account-school',
+          url: '/sertificates',
+        },
         {title: 'Works', icon: 'mdi-human-dolly', url: '/works'},
         {title: 'Apple', icon: 'mdi-apple', url: '/apple'},
         {title: 'Nest', icon: 'mdi-web', url: '/nest'},
--- src/components/NestComp.vue
+++ src/components/NestComp.vue
@@ -1,8 +1,21 @@
 <template>
-    <v-container>
+  <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Nest page</h1>
+        <h1 class="nest">Nest page</h1>
+        <v-btn class="success ml-5 mb-5 mt-5" @click="show = !show"
+          >Описание/Скрыть</v-btn
+        >
+        <transition name="anm">
+          <p class="mt-5" v-if="show">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            explicabo quibusdam nisi reprehenderit optio, numquam inventore
+            error repellendus sed illum recusandae non maiores, facere dolorum
+            similique laborum expedita ducimus tempore.
+             <!-- <v-btn class="success ml-12">Смотреть сайт</v-btn> -->
+          </p>
+          <v-btn class="success ml-12">Смотреть сайт</v-btn>
+        </transition>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,10 +23,45 @@
 
 <script>
 export default {
-    name: 'AppNest'
+  name: 'AppNest',
+  data() {
+    return {
+      show: false,
+    }
+  },
 }
 </script>
 
-<style>
+<style scoped>
+.nest {
+  text-align: center;
+  margin-top: 50px;
+}
+.anm-enter {
+  opacity: 0;
+}
+.anm-enter-active {
+  transition: opacity 1s;
+}
+.anm-enter-to {
+}
 
+.anm-leave {
+}
+.anm-leave-active {
+  animation: 1s anm-slide forwards;
+  transition: opacity 1s;
+}
+.anm-leave-to {
+  opacity: 0;
+}
+
+@keyframes anm-slide {
+  from {
+    transform: translateX(0px);
+  }
+  to {
+    transform: translateX(-100px);
+  }
+}
 </style>
